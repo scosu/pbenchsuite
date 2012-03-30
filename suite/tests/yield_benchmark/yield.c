4d4c98bc9cbf3620b472fe8e9830923b72fd2cba
Markus Pargmann <mpargmann@allfex.org>
2012-03-30 15:42:41 +0200

    yield_benchmark: count iterations

--- suite/tests/yield_benchmark/yield.c
+++ suite/tests/yield_benchmark/yield.c
@@ -1,31 +1,37 @@
 #include <sched.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-long iterations;
+int shutdown;
 
 void *yielder(void *data) {
-	long i = iterations;
-	while (i--) {
+	unsigned long i = 0;
+	while (!shutdown) {
 		sched_yield();
+		++i;
 	}
-	return NULL;
+	return (void*)i;
 }
 
 
 int main(int argc, char **argv) {
 	int threads = atoi(argv[2]);
 	long i = 0;
+	unsigned long long iterations = 0;
 	pthread_t *pthreads = malloc(sizeof(pthread_t) * threads);
-	iterations = atol(argv[1]);
-	iterations = iterations / threads;
 	for (; i != threads; ++i) {
 		pthread_create(pthreads + i, NULL, yielder, NULL);
 	}
+	sleep(atol(argv[1]));
+	shutdown = 1;
 	for (i = 0; i != threads; ++i) {
 		void *ret;
 		pthread_join(pthreads[i], &ret);
+		iterations += (unsigned long)ret;
 	}
+	printf("%llu\n", iterations);
 	return 0;
 }
 
