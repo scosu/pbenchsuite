e0e9233dd896ae8a574db97f460e22f21550087a
Markus Pargmann <mpargmann@allfex.org>
2012-05-27 15:50:34 +0200

    Plotter utils

---
+++ suite/plotters/data_loader.py
@@ -0,0 +1,349 @@
+
+import json
+import os
+import re
+import sys
+
+
+# filtered_arg is a list of lists. The sublists express the argument we match
+# as a regular expression
+def filter_matcher(filtered_arg, args, no_regex = False):
+	matched_filters = 0
+	for f in filtered_arg:
+		matching = 0
+		for arg in args:
+			if (no_regex and f[matching] == str(arg)) or (not no_regex and re.match(f[matching], str(arg))):
+				matching += 1
+				if matching == len(f):
+					matched_filters += 1
+					break
+	return matched_filters == len(filtered_arg)
+
+def filter_options(options, pre_filters=None, filters=None, post_filters=None, monitors=None, no_regex=False):
+	matches = 0
+	if pre_filters == None:
+		matches += 1
+	else:
+		if filter_matcher(pre_filters, options['pre_args'], no_regex):
+			matches += 1
+	if filters == None:
+		matches += 1
+	else:
+		if filter_matcher(filters, options['args'], no_regex):
+			matches += 1
+	if post_filters == None:
+		matches += 1
+	else:
+		if filter_matcher(post_filters, options['post_args'], no_regex):
+			matches += 1
+	if monitors == None:
+		matches += 1
+	else:
+		if 'monitors' in options:
+			match_fail = False
+			for mon in monitors:
+				if mon not in options['monitors']:
+					match_fail = True
+					break
+			if not match_fail:
+				matches += 1
+
+	if matches == 4:
+		return True
+	return False
+
+def deep_equal(a, b):
+	types = [dict, list, str, float, int]
+	for t in types:
+		if isinstance(a, t) and not isinstance(b, t):
+			return False
+	if isinstance(a, dict):
+		if not deep_equal(a.keys(), b.keys()):
+			return False
+		for k in a.keys():
+			if not deep_equal(a[k], b[k]):
+				return False
+	elif isinstance(a, list):
+		if len(a) != len(b):
+			return False
+		for i in range(0, len(a)):
+			if not deep_equal(a[i], b[i]):
+				return False
+	elif isinstance(a, float) or isinstance(a, int) or isinstance(a, str):
+		if a != b:
+			return False
+	return True
+
+def arg_diff(a,b):
+	diff = []
+	for i in a:
+		if i in b:
+			continue
+		if i in diff:
+			continue
+		diff.append(i)
+	for i in b:
+		if i in a:
+			continue
+		if i in diff:
+			continue
+		diff.append(i)
+	return diff
+
+def _add_var_set(bigset, diff):
+	equal = False
+	for i in bigset:
+		if deep_equal(i, diff):
+			equal = True
+			break
+	if not equal:
+		bigset.append(diff)
+
+def get_args_not_in_set(args, argset):
+	diff = []
+	last_i = None
+	ct = 0
+	while ct < len(args):
+		if args[ct].startswith('-') and ct < len(args)-1 and not args[ct+1].startswith('-'):
+			for arg in argset:
+				rang = max(0, len(arg) - 1)
+				not_found = True
+				for ct2 in range(0, rang):
+					if args[ct] == arg[ct2] and args[ct+1] == arg[ct2+1]:
+						not_found = False
+						break
+				if not_found:
+					d = [args[ct], args[ct+1]]
+					if d not in diff:
+						diff.append(d)
+					ct += 1
+					break
+		elif args[ct] not in diff:
+			for arg in argset:
+				if args[ct] not in arg:
+					if args[ct] not in diff:
+						diff.append(args[ct])
+					break
+		ct += 1
+	return diff
+
+def arg_variation(argset):
+	pre_args = []
+	args = []
+	post_args = []
+	for i in argset:
+		pre_args.append(i[0])
+		args.append(i[1])
+		post_args.append(i[2])
+	diffs = []
+	for i in argset:
+		diff = []
+		diff.append(get_args_not_in_set(i[0], pre_args))
+		diff.append(get_args_not_in_set(i[1], args))
+		diff.append(get_args_not_in_set(i[2], post_args))
+		diffs.append(diff)
+	return diffs
+
+	variations = []
+	for args in argset:
+		diff_pre = []
+		diff_post = []
+		diff = []
+		state = 0
+		last_state1_appended = False
+		last_i = None
+		for i in args:
+			if state == 0 and i.startswith('-'):
+				state = 1
+			elif state == 1 and not i.startswith('-'):
+				state = 2
+			elif state == 2 or state == 3:
+				if i.startswith('-'):
+					state = 1
+				else:
+					state = 3
+			for arg in argset:
+				if i not in arg:
+					if state == 0:
+						if i not in diff_pre:
+							diff_pre.append(i)
+					elif state == 2:
+						if last_i not in diff:
+							diff.append(last_i)
+							diff.append(i)
+					elif state == 3:
+						if i not in diff_post:
+							diff_post.append(i)
+			last_i = i
+		diff_opt = []
+		state = 0
+		last = None
+		for i in range(0, len(diff)):
+			if state == 1:
+				if diff[i].startswith('-'):
+					if last != None:
+						diff_opt.append([last])
+						last = None
+					state = 0
+				else:
+					diff_opt.append([last, diff[i]])
+					last = None
+			if state == 0:
+				if diff[i].startswith('-'):
+					state = 1
+					last = diff[i]
+				else:
+					diff_opt.append([diff[i]])
+		_add_var_set(variations, diff_pre + diff_opt + diff_post)
+
+
+	return variations
+
+class loader:
+	def __init__(self):
+		self.data = {}
+		self.systems = {}
+	def get_benchname(self, filepath):
+		f = open(filepath, 'r')
+		data = json.load(f)
+		f.close()
+		return data['bench']['name']
+	def get_benchnames_dir(self, directory):
+		result = set()
+		for dir_item in os.listdir(directory):
+			abs_path = os.path.join(directory, dir_item)
+			if not os.path.isfile(abs_path):
+				continue
+			try:
+				result.add(self.get_benchname(abs_path))
+			except:
+				continue
+		return list(result)
+	def get_benchnames_pathes(self, pathes):
+		result = set()
+		for i in pathes:
+			print(i)
+			if os.path.isfile(i):
+				try:
+					result.add(self.get_benchname(i))
+				except:
+					print("load file exception: " + sys.exc_info()[0])
+					continue
+			elif os.path.isdir(i):
+				result = result.union(self.get_benchnames_dir(i))
+		return list(result)
+	def _get_system(self, runname, sysinfo):
+		if runname in self.systems:
+			if deep_equal(sysinfo, self.systems[runname]):
+				return runname
+			while runname in self.systems:
+				runname = runname + "_"
+		self.systems[runname] = sysinfo
+		return runname
+
+	def load_file(self, f, only_benchs=None, only_instances=None, pre_filters=None, filters=None, post_filters=None, monitors=None):
+		try:
+			f = open(f, 'r')
+			data = json.load(f)
+			f.close()
+			bench_name = data['bench']['name']
+			instance_name = data['instance']['name']
+			if only_benchs != None and bench_name not in only_benchs:
+				return False
+			if only_instances != None and instance_name not in only_instances:
+				return False
+			if not filter_options(data['instance']['options'], pre_filters, filters, post_filters, monitors):
+				return False
+			if bench_name not in self.data:
+				self.data[bench_name] = {}
+			system = self._get_system(data['suite']['runname'], data['system'])
+			if instance_name not in self.data[bench_name]:
+				self.data[bench_name][instance_name] = {}
+			self.data[bench_name][instance_name][system] = data
+			return True
+		except:
+			print("load exception: " + sys.exc_info()[0])
+			return False
+	def load_dir(self, directory, only_benchs=None, only_instances=None):
+		try:
+			for dir_item in os.listdir(directory):
+				abs_path = os.path.join(directory, dir_item)
+				if not os.path.isfile(abs_path):
+					continue
+				self.load_file(abs_path, only_benchs, only_instances)
+		except:
+			return False
+	def load_path(self, path, only_benchs=None, only_instances=None):
+		if os.path.isfile(path):
+			return self.load_file(path, only_benchs, only_instances)
+		elif os.path.isdir(path):
+			return self.load_dir(path, only_benchs, only_instances)
+		else:
+			print("Error: " + path + " is not a directory and not a file")
+			return False
+	def load_pathes(self, pathes, only_benchs=None, only_instances=None):
+		success = True
+		for i in pathes:
+			s = self.load_path(i, only_benchs, only_instances)
+			success = s and success
+		return success
+	def get_data(self):
+		return self.data
+
+	# pre_filters, filters and post_filters have to be a list of lists of
+	# regular expressions. The result is accepted as match if all filters
+	# match
+	def get_filtered_data(self, bench=None, pre_filters=None, filters=None, post_filters=None, monitors=None, no_regex=False):
+		matched = {}
+		if bench != None:
+			for instancename,instance in self.data[bench].items():
+				for runname, run in instance.items():
+					if filter_options(run['instance']['options'], pre_filters, filters, post_filters, monitors, no_regex=no_regex):
+						if instancename not in matched:
+							matched[instancename] = {}
+						matched[instancename][runname] = run
+		else:
+			for benchname, benchobj in self.data.items():
+				for instancename, instance in benchobj.items():
+					for runname, run in instance.items():
+						if filter_options(run['instance']['options'], pre_filters, filters, post_filters, monitors, no_regex=no_regex):
+							if benchname not in matched:
+								matched[benchname] = {}
+							if instancename not in matched[benchname]:
+								matched[instancename] = {}
+							matched[benchname][instancename][runname] = instance
+
+		return matched
+	def get_arg_variation(self, bench):
+		argset_pre = []
+		argset = []
+		argset_post = []
+		for instname, inst in self.data[bench].items():
+			for sysname, system in inst.items():
+				arg_sum = []
+				args = system['instance']['options']['args']
+				args_pre = system['instance']['options']['post_args']
+				args_post = system['instance']['options']['pre_args']
+				if args_pre != None:
+					arg_sum.append(args_pre)
+				else:
+					arg_sum.append([])
+				if args != None:
+					arg_sum.append(args)
+				else:
+					arg_sum.append([])
+				if args_post != None:
+					arg_sum.append(args_post)
+				else:
+					arg_sum.append([])
+				argset.append(arg_sum)
+		return arg_variation(argset)
+
+if __name__ == '__main__':
+	l = loader()
+	l.load_path('/mnt/local_storage/pbench/cfs_test')
+	print(l.get_argvariation('hackbench'))
+	print(l.get_argvariation('kernel_compile'))
+	print(l.get_argvariation('unixbench'))
+	print(l.get_argvariation('postfix_msg_passing'))
+	print(l.get_argvariation('yield_benchmark'))
---
+++ suite/plotters/plot_utils.py
@@ -0,0 +1,273 @@
+#!/usr/bin/python2
+
+import matplotlib.pyplot as plt
+import scipy
+import scipy.stats
+import numpy
+import operator
+
+
+def mean_confidence_interval(data, confidence=0.95):
+
+	a = 1.0*numpy.array(data)
+	n = len(a)
+	m, se = numpy.mean(a), scipy.stats.sem(a)
+	# calls the inverse CDF of the Student's t distribution
+	h = se * scipy.stats.t._ppf((1+confidence)/2., n-1)
+	return m, h
+
+
+##
+# @brief Draw a bar chart in grouped form.
+#    _         _
+#  _| |       | |
+# | | |_   _ _| |
+# | | | | | | | |
+#  a b c   a b c
+#    1       2
+#
+#
+# @param data Dict of dicts in following format:
+# You can replace the floats on the deepest level by lists of floats for stacked
+# bars.
+# @param available_sub_keys
+#
+# @return
+
+def _sort_keys(keys):
+	fkeys = []
+	skeys = []
+	for i in keys:
+		try:
+			float(i)
+			fkeys.append(i)
+		except:
+			skeys.append(i)
+	return sorted(fkeys, key=float) + sorted(skeys)
+
+def _plot_stuff(title = None, x_label = None, y_label = None, no_legend = False, max_x = None):
+	if title != None:
+		plt.title(title)
+	if x_label != None:
+		plt.xlabel(x_label)
+	if y_label != None:
+		plt.ylabel(y_label)
+	if not no_legend and max_x != None:
+		handles, labels = plt.gca().get_legend_handles_labels()
+		hl = sorted(zip(handles, labels), key=operator.itemgetter(1))
+		handles, labels = zip(*hl)
+		max_label = 0
+		for k in labels:
+			max_label = max(len(k), max_label)
+		plt.xlim(xmax = max(max_x + 1, max_x * (1 + (max(0, max_label - 3)) * 0.06)))
+		plt.legend(handles, labels, loc='center right', bbox_to_anchor=(1.13, 0.5), fancybox=True, shadow=True)
+
+# Takes something like this:
+#data_line = {
+#		'x': [(0,1), (1,3), (2,2), (3,5)],
+#		'y': [(0,1), (2,1), (3,2), (4,5)],
+#		'z': [(0,[1,2,3,2,2,2,2]), (1,[2,3,2,2,3,3,3,3]), (2,2), (3,5)]
+#}
+def plot_line_chart(data, x_keys=None, title=None, y_label=None, x_label=None, no_legend=False, confidence_arg=0.95, fmts_arg=None):
+	global fmts
+	global fmtsid
+	if fmts_arg == None:
+		fmts = [
+				{'color': 'b', 'linestyle': '-', 'marker': ','},
+				{'color': 'r', 'linestyle': '-', 'marker': '.'},
+				{'color': 'g', 'linestyle': '-', 'marker': '+'},
+				{'color': 'b', 'linestyle': '--', 'marker': 'o'},
+				{'color': 'c', 'linestyle': '--', 'marker': 'D'},
+				{'color': 'y', 'linestyle': '--', 'marker': '^'}]
+	else:
+		fmts = fmts_arg
+	fmtsid = 0
+	def _next_fmt():
+		global fmtsid
+		fmt = fmts[fmtsid%len(fmts)]
+		fmtsid += 1
+		return fmt
+
+	if x_keys == None:
+		x_keys = _sort_keys(data.keys())
+
+	max_overall_x = None
+	min_overall_x = None
+	for k in x_keys:
+		if k not in data:
+			continue
+		v = data[k]
+		xs = []
+		ys = []
+		yerrs = []
+		yerr_not_zero = False
+		for pt in v:
+			xs.append(pt[0])
+			if max_overall_x == None:
+				max_overall_x = pt[0]
+				min_overall_x = pt[0]
+			else:
+				max_overall_x = max(max_overall_x, pt[0])
+				min_overall_x = min(min_overall_x, pt[0])
+			if isinstance(pt[1], list):
+				m, h = mean_confidence_interval(pt[1])
+				yerrs.append(h)
+				ys.append(m)
+				yerr_not_zero = True
+			else:
+				ys.append(pt[1])
+				yerrs.append(0.0)
+		fmt = _next_fmt()
+		if yerr_not_zero:
+			plt.errorbar(xs, ys, yerrs, label=k, linestyle=fmt['linestyle'], marker=fmt['marker'], color=fmt['color'])
+		else:
+			plt.plot(xs, ys, label=k, linestyle=fmt['linestyle'], marker=fmt['marker'], color=fmt['color'])
+	rang = max_overall_x - min_overall_x
+	plt.xlim(xmin = (min_overall_x - rang*0.05))
+	_plot_stuff(title=title, x_label=x_label, y_label=y_label, no_legend=no_legend, max_x=(max_overall_x + rang*0.05))
+
+
+# Works with all those structures:
+#data = {'a':1, 'b':4, 'c':2}
+#data = {
+#		'a': {'1': 2, '2': 1, 'dummy': 4},
+#		'b': {'1': 3, '2': 1, 'dummy': 3},
+#		'c': {'1': 4, '2': 2, 'dummy': 1}
+#}
+#data = {
+#		'x': {'1': {'a': 4, 'e': [1, 2, 3], 'c': 3}, '2': {'d':5, 'e': 2}, 'dummy': {'e':3}},
+#		'y': {'1': {'a': 6, 'e': [1, 2, 2, 3], 'c': 1}, '2ab': {'d':5}, 'dummy': {'e':3}},
+#		'z': {'1': {'a': 2, 'eaasd': [1, 2, 2, 2, 3], 'c': 4}, '2': {'d':5}, 'dummy': {'e':3}}
+#}
+##
+# @brief Plot a bar chart with data of up to 3 dimensions
+#
+# @param data maximal 3 level depth of dictionary with lists of floats or floats
+# as lowest children. Lists will produce errorbars. The keys of the dictionary
+# are used to sort the stuff.
+# @param l1_keys
+# @param l2_keys
+# @param l3_keys
+# @param colors_arg
+# @param confidence_arg
+#
+# @return
+def plot_bar_chart(data, l1_keys=None, l2_keys=None, l3_keys=None, colors_arg=None, confidence_arg=0.95, no_legend=False, title=None, y_label=None, x_label=None):
+#Many global hacks because python 2.7 doesn't support nonlocal
+	if l1_keys == None:
+		l1_keys = _sort_keys(data.keys())
+	if l2_keys == None:
+		l2_keys = set()
+		for k1, v1 in data.items():
+			if not isinstance(v1, dict):
+				continue
+			for k2, v2 in v1.items():
+				l2_keys.add(k2)
+		l2_keys = _sort_keys(list(l2_keys))
+	if l3_keys == None:
+		l3_keys = set()
+		for k1, v1 in data.items():
+			if not isinstance(v1, dict):
+				continue
+			for k2, v2 in v1.items():
+				if not isinstance(v2, dict):
+					continue
+				for k3, v3 in v2.items():
+					l3_keys.add(k3)
+		l3_keys = _sort_keys(list(l3_keys))
+	xxticks = []
+	xticks = []
+	global confidence
+	confidence = confidence_arg
+	global label_colors
+	label_colors = {}
+	global colors
+	if colors_arg == None:
+		colors = ['0.3', '0.5', '0.7', '0.4', '0.6', '0.8']
+	else:
+		colors = colors_arg
+	global colorid
+	colorid = 0
+	global x
+	x = 1
+	global max_overall
+	max_overall = 0
+	def _xtick(label, x_val = None):
+		global x
+		if x_val == None:
+			x_val = x
+		xxticks.append(x_val)
+		xticks.append(label)
+	def _color_get(label):
+		global colorid
+		if label in label_colors:
+			return False, label_colors[label]
+		else:
+			label_colors[label] = colors[colorid % len(colors)]
+			colorid += 1
+			return True, label_colors[label]
+	def _plt_bar(x, y, label):
+		global confidence
+		set_label, color = _color_get(label)
+		if isinstance(y, list):
+			global max_overall
+			m,h = mean_confidence_interval(y, confidence)
+			max_overall = max(m, max_overall)
+			if label == None or not set_label:
+				plt.bar(x, m, align="center", color = color, ecolor='r', yerr = h)
+			else:
+				plt.bar(x, m, align="center", color = color, ecolor='r', label = label, yerr = h)
+		else:
+			max_overall = max(max_overall, y)
+			if label == None or not set_label:
+				plt.bar(x, y, align="center", color = color)
+			else:
+				plt.bar(x, y, align="center", color = color, label = label)
+
+	levels = 0
+	for l1_key in l1_keys:
+		l1_val = data[l1_key]
+		levels = 1
+		if not isinstance(l1_val, dict):
+			_xtick(l1_key)
+			_plt_bar(x, l1_val, l1_key)
+			x += 1
+		else:
+			_xtick(l1_key, x + int(len(l1_val.keys()) / 2))
+			levels = 2
+			for l2_key in l2_keys:
+				if l2_key not in l1_val:
+					continue
+				l2_val = l1_val[l2_key]
+				levels = 3
+				if not isinstance(l2_val, dict):
+					_plt_bar(x, l2_val, l2_key)
+					max_val = 0
+					x += 1
+
+				else:
+					max_val = 0
+					for l3_key in l3_keys:
+						if l3_key not in l2_val:
+							continue
+						l3_val = l2_val[l3_key]
+						if isinstance(l3_val, list):
+							max_val = max(sum(l3_val)/len(l3_val), max_val)
+						else:
+							max_val = max(l3_val, max_val)
+						_plt_bar(x, l3_val, l3_key)
+
+					rot = min((len(l2_key) - 1)*30, 90)
+					plt.text(x, max_val*1.02, l2_key, horizontalalignment='center', verticalalignment='bottom', rotation=rot)
+					x += 1
+			x += 1
+	if levels == 3:
+		plt.ylim(ymin = 0, ymax=1.2*max_overall)
+	else:
+		plt.ylim(ymin = 0)
+	plt.xticks(xxticks, xticks)
+	_plot_stuff(x_label = x_label, y_label = y_label, title = title, no_legend = no_legend, max_x = x-1)
+
+
+
+
